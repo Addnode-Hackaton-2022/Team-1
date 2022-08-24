fa9ceb931ffe884f6633ed136684ae907ca9b753
MStalvik <89071163+MStalvik@users.noreply.github.com>
2022-08-24 17:57:07 +0200

    Initial commit

---
+++ SSRSWebApi/Common/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using SSRSWebApi.Models;
+
+namespace SSRSWebApi.Common
+{
+    public static class Extensions
+    {
+        public static bool IsReadOnly(this AttributeTypes attribute)
+        {
+            switch (attribute)
+            {
+                case AttributeTypes.TankLevel: return true;
+                case AttributeTypes.AlarmLevel: return false;
+                default: return false;
+            }          
+        }
+        public static bool MergeAttribute(this BoatAttribute sourceAttribute, BoatAttribute newAttribute)
+        {
+            if (sourceAttribute.Timestamp < newAttribute.Timestamp)
+            {
+                sourceAttribute.Value = newAttribute.Value;
+                sourceAttribute.Timestamp = newAttribute.Timestamp;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ SSRSWebApi/Controllers/MonitorController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SSRSWebApi.Common;
+using SSRSWebApi.Domain;
+using SSRSWebApi.Models;
+
+namespace SSRSWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MonitorController : ControllerBase
+    {
+        [HttpGet]
+        public List<BoatModel> GetBoats([FromQuery] string boatIds)
+        {
+            var result = new List<BoatModel>();
+            var idList = boatIds.Split(',').ToList();
+            foreach(var id in idList)
+            {
+                if (InmemoryStorage.Boats.TryGetValue(id, out var boat))
+                {
+                    result.Add(boat);
+                }
+            }
+            return result;
+        }
+
+        [HttpPost]
+        [Route("setattribute")]
+        public void SetValue([FromBody] SetAttributeRequest request)
+        {
+            if (!request.Attribute.Type.IsReadOnly())
+            {
+                if (InmemoryStorage.Boats.TryGetValue(request.BoatId, out var boat))
+                {
+                    var currentAttribute = boat.BoatAttributes.FirstOrDefault(x => x.Type == request.Attribute.Type);
+                    if (currentAttribute?.Timestamp < request.Attribute.Timestamp && currentAttribute?.Value != request.Attribute.Value)
+                    {
+                        currentAttribute.Value = request.Attribute.Value;
+                        currentAttribute.Timestamp = request.Attribute.Timestamp;
+                    }
+                }
+            }
+        }
+
+    }
+}
